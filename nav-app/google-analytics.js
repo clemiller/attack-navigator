6f5c6180bdc19f4e6406b2c2f08199a5bd5ca47c
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-03-18 12:08:02 -0400

    test

--- nav-app/google-analytics.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const target = './src/environments/environment.prod.ts';
-const config = `export const environment = {
-	production: true,
-	GOOGLE_ANALYTICS: '${process.env.GOOGLE_ANALYTICS}'
-};`;
-fs.writeFile(target, config, function(err) {
-	if (err) console.log(err);
-});
